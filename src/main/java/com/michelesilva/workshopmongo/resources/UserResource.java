2c3ec86c4380d149b71b7fa28b20d1f6760ccc32
michelesantosl1 <misanttos300@gmail.com>
2024-05-12 15:02:20 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/michelesilva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/michelesilva/workshopmongo/resources/UserResource.java
@@ -42,4 +42,10 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+ 	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
