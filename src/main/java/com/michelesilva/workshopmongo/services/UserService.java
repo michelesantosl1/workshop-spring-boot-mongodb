34eb4d073f530648abbb8ff5428adb374cd50464
michelesantosl1 <misanttos300@gmail.com>
2024-05-12 14:06:49 -0300

    Obtendo um usuário por id

--- src/main/java/com/michelesilva/workshopmongo/services/UserService.java
+++ src/main/java/com/michelesilva/workshopmongo/services/UserService.java
@@ -1,22 +1,27 @@
 package com.michelesilva.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.michelesilva.workshopmongo.domain.User;
 import com.michelesilva.workshopmongo.repository.UserRepository;
+import com.michelesilva.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
 	@Autowired
 	private UserRepository repo;
 	
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
-		
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	//return user;
 }
---
+++ src/main/java/com/michelesilva/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.michelesilva.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
